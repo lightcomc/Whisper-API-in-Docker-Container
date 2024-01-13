0a203f85defdd7576df0923e4f65b05919b8df3a
lightcomc <111423897+lightcomc@users.noreply.github.com>
2024-01-13 22:38:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ torch.cuda.is_available()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load the Whisper model:
-model = whisper.load_model("base", device=DEVICE)
+model = whisperx.load_model("base", device=DEVICE)
 
 app = Flask(__name__)
 
